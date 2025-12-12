eb59dfc68153d518510649cdea1ebe8982ac0657
pumca2025 <pumca2025@gmail.com>
2025-12-12 16:34:21 +0530

    add

--- src/components/herosection.jsx
+++ src/components/herosection.jsx
@@ -1,28 +1,59 @@
 import React from 'react'
 import { Typewriter } from "react-simple-typewriter";
+import BlurText from './aminations/BlurText';
+import DotGrid from './aminations/DotGrid';
 const herosection = () => {
+  const handleAnimationComplete = () => {
+    console.log('Animation completed!');
+  };
   return (
-    
+
     <>
-    <div className="hero">
-        <h1>Hello 👋 , I'm B Sam</h1>
-      <h2>
-        <Typewriter
-          words={[
-            "MERN Stack Developer",
-            "Tech Enthusiast",
-            "Computer Science Graduate",
-            "Lifelong Learner"
-          ]}
-          loop={true}
-          cursor
-          cursorStyle="|"
-          typeSpeed={100}
-          deleteSpeed={50}
-          delaySpeed={2000}
+    
+
+<div style={{ width: '100%', height: '600px', position: 'relative' }}>
+  <DotGrid
+    dotSize={5}
+    gap={15}
+    baseColor="#5227FF"
+    activeColor="#5227FF"
+    proximity={120}
+    shockRadius={250}
+    shockStrength={5}
+    resistance={750}
+    returnDuration={1.5}
+  />
+
+      <div className="hero">
+
+
+        <BlurText
+          text="Hello 👋 , I'm B Sam"
+          delay={150}
+          animateBy="words"
+          direction="top"
+          onAnimationComplete={handleAnimationComplete}
+          className="name"
         />
-      </h2>
-      <button onClick={() => window.scrollTo({ top: document.getElementById("projects").offsetTop, behavior: "smooth" })}>See My Work</button>
+        
+        <h2>
+          <Typewriter
+            words={[
+              "MERN Stack Developer",
+              "Tech Enthusiast",
+              "Computer Science Graduate",
+              "Lifelong Learner"
+            ]}
+            loop={true}
+            cursor
+            cursorStyle="|"
+            typeSpeed={100}
+            deleteSpeed={50}
+            delaySpeed={2000}
+          />
+        </h2>
+        <button className='work' onClick={() => window.scrollTo({ top: document.getElementById("projects").offsetTop, behavior: "smooth" })}>See My Work</button>
+      </div>
       </div>
     </>
   )
